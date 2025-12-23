462776bd92cf29449cf3e7b38cbb2fb3273ac475
Ajayi AKINSOLA MARTINS <145346225+martinsAkin@users.noreply.github.com>
2025-12-23 13:02:11 +0100

    Update grid layout to include four columns on large screens

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -34,7 +34,7 @@ export const SolutionCard = () => {
 
  return (
   <>
-   <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center justify-self-center">
+   <div className="grid grid-cols-1 md:grid-cols-2 lg: grid-cols-4 gap-6 justify-center justify-self-center">
     {cardData.map((details, index) => (
      <div
       key={index}
